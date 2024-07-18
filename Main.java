096895fb682dfab7483fdf2fb5adf4b2ec9086a1
ajayroll1 <ajaymahto2580@gmail.com>
2024-07-19 00:04:13 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+  public static void main(String[] args) {
+      //OUTPUT
+      System.out.println("this is the 3rd output\nfrom apni kaksha ");
+      System.out.println("this is the 2nd output\n");
+      System.out.print("this is the 3rd output");
+
+  }
+}
+
---
+++ StarPattern.java
@@ -0,0 +1,14 @@
+public class StarPattern {
+  public static void main(String[] args) {
+    System.out.println("*");
+    System.out.println("**");
+    System.out.println("***");
+    System.out.println("****");
+    System.out.println("*****");
+
+
+    
+    System.out.println("*\n**\n***\n****");
+
+  }
+}
